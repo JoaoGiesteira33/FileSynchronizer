43c7b55b6b3d1bcd85a5f9eab74db4a314ff024b
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2021-12-21 14:42:44 +0000

    Ola

--- Server.java
+++ Server.java
@@ -43,7 +43,7 @@ public class Server implements Runnable{
                 System.out.println("GOING TO PRINT DATA-----------------------");
                 received_m.printData();
                 //UNICA COISA Q PODE ESTAR A FALHAR, TANTO QUANTO SABEMOS
-                String file_path =  new String(Arrays.copyOfRange(received_m.getData(),1,file_size));
+                String file_path =  new String(Arrays.copyOfRange(received_m.getData(),1,file_size+1));
                 System.out.println("FILE PATH SIZE: " + file_path.length());                
                 System.out.println("FILE PATH: " + file_path);
 
